7ec17039534e8f0cce7bed43ba1e60e98f327b3c
ghbooth12 <ghbooth12@users.noreply.github.com>
2016-12-05 12:54:36 -0600

    Fix project build with Rob (#1)
    * Fix project build with Rob
    
    * Add tests for call using rack-test and test-unit

---
+++ bloc_works_test.rb
@@ -0,0 +1,23 @@
+require_relative 'lib/bloc_works'
+
+require 'test/unit'
+require 'rack/test'
+
+class IndexTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    BlocWorks::Application.new
+  end
+
+  def test_it_is_a_200
+    get '/'
+    assert_equal 200, last_response.status
+  end
+
+  def test_it_says_hello
+    get '/'
+    assert last_response.ok?
+    assert_equal 'Hello Blocheads!', last_response.body
+  end
+end
--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -2,6 +2,9 @@ require "bloc_works/version"
 
 module BlocWorks
   class Application
+    # "call" returns an array containing an HTTP status code, an HTTP header, and the text to display in the browser.
+    # Rack objects must return this triplet(an array of extactly three itmes).
+    # Rack object usually takes the "env" variable, which represents the rack environment.
     def call(env)
       [200, {'Content-Type' => 'text/html'}, ["Hello Blocheads!"]]
     end
